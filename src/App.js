bda46447ef1a3e95063858a035d55abe672d6b63
Felipe Nascimento <flpnascto@gmail.com>
2021-05-08 15:41:16 -0300

    alterar estilos da aplicação

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Provider from './context/Provider'
 import { BrowserRouter } from 'react-router-dom';
 import Router from './Routes';
-import './App.css';
+// import './App.css';
 
 function App() {
   return (
